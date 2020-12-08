c36ec491a3ec7504e5b32aebaa5d18e37698e2bf
Anonymous <anon@ymo.us>
2020-12-08 18:27:08 +0100

    working on small input

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ fn main() {
     if let Ok(lines) = read_lines("./inputs/input_day6.txt") {
         let mut groups: Vec<HashSet<char>> = Vec::new();
         let mut group: HashSet<char> = HashSet::new();
+        let mut first = true;
         for line in lines {
             if let Ok(line) = line {
                 //println!("line: {}", line);
@@ -21,12 +22,22 @@ fn main() {
                     println!("group set: {:?}", group);
                     groups.push(group);
                     group = HashSet::new();
+                    first = true;
                     continue;
                 }
                 
+                let mut person: HashSet<char> = HashSet::new();
                 for char in line.chars() {
-                    group.insert(char);
+                    person.insert(char);
                 }
+                println!("\tperson {:?}", person);
+                if first {
+                    group = person;
+                }
+                else {
+                    group = group.intersection(&person).cloned().collect();
+                }
+                first = false;
             }
         }
         println!("group set: {:?}", group);
