85b6fa94f430ba15d8aac29a7a5ad3dbefaab13a
Anonymous <anon@ymo.us>
2020-12-08 18:31:22 +0100

    read file

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{fs::File};
 use std::io::{self, BufRead};
 use std::path::Path;
 
-use std::collections::HashSet;
+
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
@@ -12,43 +12,10 @@ where P: AsRef<Path>, {
 
 fn main() {
     if let Ok(lines) = read_lines("./inputs/input_day6.txt") {
-        let mut groups: Vec<HashSet<char>> = Vec::new();
-        let mut group: HashSet<char> = HashSet::new();
-        let mut first = true;
         for line in lines {
             if let Ok(line) = line {
-                //println!("line: {}", line);
-                if line.is_empty() {
-                    println!("group set: {:?}", group);
-                    groups.push(group);
-                    group = HashSet::new();
-                    first = true;
-                    continue;
-                }
-                
-                let mut person: HashSet<char> = HashSet::new();
-                for char in line.chars() {
-                    person.insert(char);
-                }
-                println!("\tperson {:?}", person);
-                if first {
-                    group = person;
-                }
-                else {
-                    group = group.intersection(&person).cloned().collect();
-                }
-                first = false;
+                println!("line: {}", line);
             }
         }
-        println!("group set: {:?}", group);
-        groups.push(group);
-
-
-        let mut sum = 0;
-        for group in groups {
-            sum += group.len();
-        }
-
-        println!("sum: {}", sum);
     }
 }
