c9d160fddc5e095d39d7e68626ff0d5ad81c502f
Anonymous <anon@ymo.us>
2020-12-10 18:55:11 +0100

    work on very small example

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::VecDeque, fs::File};
+use std::{collections::{HashMap}, fs::File};
 use std::io::{self, BufRead};
 use std::path::Path;
 
@@ -13,11 +13,30 @@ where P: AsRef<Path>, {
 
 fn main() {
     if let Ok(lines) = read_lines("./inputs/input_day10.txt") {
-        let nums: Vec<u64> = lines.into_iter().map(|e| e.unwrap().parse::<u64>().unwrap()).collect();
-        let mut nums_iter = nums.iter();
+        let mut nums: Vec<u64> = lines.into_iter().map(|e| e.unwrap().parse::<u64>().unwrap()).collect();
+        nums.insert(0, 0);
+        nums.sort();
 
-        for &num in nums_iter {
-            println!("{}", num);
+        let mut diff_count: HashMap<u64, usize> = HashMap::new();
+        let mut iter_nums = nums.into_iter().peekable();
+        while let Some(num) = iter_nums.next() {
+            let next = iter_nums.peek();
+            match next {
+                None => {}
+                Some(&next) => {
+                    let diff = next - num;
+                    if let Some(count) = diff_count.get_mut(&diff) {
+                        *count += 1;
+                    }
+                    else {
+                        diff_count.insert(diff, 1);
+                    }
+                }
+            }
         }
+
+        let c1 = diff_count.get(&1).unwrap_or(&0);
+        let c2 = diff_count.get(&3).unwrap_or(&0);
+        println!("Diffs: {:?}, product: {} * {} = {}", diff_count, c1, c2, c1 * c2);
     }
 }
