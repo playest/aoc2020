636562eed4836fb67e44a1114a6e0b2d8e91cee9
Anonymous <anon@ymo.us>
2020-12-10 20:05:59 +0100

    works on medium example

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::{HashMap}, fs::File};
+use std::{fs::File};
 use std::io::{self, BufRead};
 use std::path::Path;
 
@@ -17,6 +17,10 @@ fn factorial(num: u64) -> u64 {
     }
 }
 
+fn combination(n: u64, r: u64) -> u64 {
+    factorial(n) / (factorial(r) * factorial(n - r))
+}
+
 fn main() {
     if let Ok(lines) = read_lines("./inputs/input_day10.txt") {
         let mut nums: Vec<u64> = lines.into_iter().map(|e| e.unwrap().parse::<u64>().unwrap()).collect();
@@ -26,10 +30,9 @@ fn main() {
         println!("{:?}", nums);
 
         let mut previous: Option<u64> = None;
-        let mut diff_count: HashMap<u64, usize> = HashMap::new();
         let mut iter_nums = nums.into_iter().peekable();
         let mut can_be_removed = 0;
-        let mut arrangments = 0;
+        let mut arrangments = 1;
         while let Some(num) = iter_nums.next() {
             if let Some(previous) = previous {
                 let next = iter_nums.peek();
@@ -43,8 +46,10 @@ fn main() {
                             println!("\t{} could be removed, consecutive: {}", num, can_be_removed);
                         }
                         else if can_be_removed != 0 {
-                            let new_arrangments = factorial(can_be_removed + 1);
-                            arrangments += new_arrangments;
+                            let new_arrangments = combination(can_be_removed, 0)
+                                + if can_be_removed >= 1 { combination(can_be_removed, 1) } else { 0 }
+                                + if can_be_removed >= 2 { combination(can_be_removed, 2) } else { 0 };
+                            arrangments *= new_arrangments;
                             println!("end of remove chain, {} new arrangements for {} total arrangements", new_arrangments, arrangments);
                             can_be_removed = 0;
                         }
@@ -53,9 +58,5 @@ fn main() {
             }
             previous = Some(num);
         }
-
-        let c1 = diff_count.get(&1).unwrap_or(&0);
-        let c3 = diff_count.get(&3).unwrap_or(&0);
-        println!("Diffs: {:?}, product: {} * {} = {}", diff_count, c1, c3, c1 * c3);
     }
 }
