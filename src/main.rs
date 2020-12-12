8ebb5013b3eda54431190849002b90429fce6eb8
Anonymous <anon@ymo.us>
2020-12-12 16:29:30 +0100

    ugly code but works on small example

--- src/main.rs
+++ src/main.rs
@@ -44,33 +44,6 @@ enum Direction {
 }
 
 impl Direction {
-    fn rotate_right(&self, deg: i32) -> Direction {
-        if deg % 90 != 0 {
-            panic!("Cannot turn of {} degrees, angle must be a multiple of 90", deg);
-        }
-        let mut step = (deg / 90) % 4;
-        let mut dir = self.clone();
-        println!("\tangle of {} degrees is {} steps right", deg, step);
-        if step < 0 {
-            step += 4;
-        }
-        while step != 0 {
-            dir = match dir {
-                Direction::North => Direction::East,
-                Direction::East => Direction::South,
-                Direction::South => Direction::West,
-                Direction::West => Direction::North,
-            };
-            step -= 1;
-        }
-        dir
-
-    }
-
-    fn rotate_left(&self, deg: i32) -> Direction {
-        self.rotate_right(-1 * deg)
-    }
-
     fn to_instruction(&self, v: i32) -> Instruction {
         match self {
             Direction::North => Instruction::North(v),
@@ -81,48 +54,105 @@ impl Direction {
     }
 }
 
-#[derive(Debug)]
-struct Ship {
-    facing: Direction,
+#[derive(Debug, Clone)]
+struct Point {
     x: i32,
     y: i32,
 }
 
-impl Ship {
-    fn new(starting_direction: Direction, x: i32, y: i32) -> Self {
-        Ship {
-            facing: starting_direction,
+impl Point {
+    fn new(x: i32, y: i32) -> Self {
+        Self {
             x,
             y,
         }
     }
 
-    fn take_order(&mut self, instruction: Instruction) {
+    fn rotate_right(&mut self, deg: i32) {
+        if deg % 90 != 0 {
+            panic!("Cannot turn of {} degrees, angle must be a multiple of 90", deg);
+        }
+        let mut step = (deg / 90) % 4;
+        println!("\tangle of {} degrees is {} steps right", deg, step);
+        if step < 0 {
+            step += 4;
+        }
+        let (x, y) = (self.x, self.y);
+        let (new_x, new_y) = match step {
+            0 => ( x,  y),
+            1 => (-y,  x),
+            2 => (-x, -y),
+            3 => ( y, -x),
+            _ => panic!("Should not get a step outside [0, 3] for rotate: {}° -> {}", deg, step),
+        };
+        self.x = new_x;
+        self.y = new_y;
+    }
+
+    fn rotate_left(&mut self, deg: i32) {
+        self.rotate_right(-1 * deg);
+    }
+
+    fn take_order(&mut self, instruction: Instruction, relative_to: &Point) {
         match instruction {
             Instruction::North(v) => {
-                //self.facing = Direction::North;
                 self.y -= v;
             },
             Instruction::South(v) => {
-                //self.facing = Direction::South;
                 self.y += v;
             },
             Instruction::East(v) => {
-                //self.facing = Direction::East;
                 self.x += v;
             },
             Instruction::West(v) => {
-                //self.facing = Direction::West;
                 self.x -= v;
             },
             Instruction::Left(v) => {
-                self.facing = self.facing.rotate_left(v);
+                self.rotate_left(v);
             },
             Instruction::Right(v) => {
-                self.facing = self.facing.rotate_right(v);
+                self.rotate_right(v);
             },
             Instruction::Forward(v) => {
-                self.take_order(self.facing.to_instruction(v));
+                self.x += relative_to.x;
+                self.y += relative_to.y;
+            },
+        };
+    }
+}
+
+#[derive(Debug)]
+struct Ship {
+    location: Point,
+    waypoint: Point,
+}
+
+impl Ship {
+    fn new(x: i32, y: i32, waypoint_x: i32, waypoint_y: i32) -> Self {
+        Self {
+            location: Point::new(x, y),
+            waypoint: Point::new(waypoint_x, waypoint_y),
+        }
+    }
+
+    fn take_order(&mut self, instruction: Instruction) {
+        match instruction {
+            Instruction::North(v) |
+            Instruction::South(v) |
+            Instruction::East(v) |
+            Instruction::West(v) |
+            Instruction::Left(v) |
+            Instruction::Right(v) => {
+                self.waypoint.take_order(instruction, &self.location);
+            },
+            Instruction::Forward(v) => {
+                let mut i = v;
+                while i > 0 {
+                    //println!("move a#{}: {:?} / {:?}", i, self.location, self.waypoint);
+                    self.location.take_order(instruction, &self.waypoint);
+                    //println!("move c#{}: {:?} / {:?}", i, self.location, self.waypoint);
+                    i -= 1;
+                }
             },
         };
     }
@@ -139,13 +169,15 @@ fn main() {
             }
         }
 
-        let mut ship = Ship::new(Direction::East, 0, 0);
+        let ship_x = 0;
+        let ship_y = 0;
+        let mut ship = Ship::new(ship_x, ship_y, ship_x + 10, ship_y - 1);
         println!("ship: {:?}", ship);
         for ins in instructions {
             println!("\tinstruction: {:?}", ins);
             ship.take_order(ins);
             println!("ship: {:?}", ship);
         }
-        println!("Manhattan distance: {}", ship.x.abs() + ship.y.abs());
+        println!("Manhattan distance: {}", ship.location.x.abs() + ship.location.y.abs());
     }
 }
